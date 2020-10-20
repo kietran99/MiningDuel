0f106229c2fe4570d779f38a5ce7760ffed38b7e
kietran99 <kietran99@gmail.com>
2020-10-20 18:23:36 +0700

    * Dock and resize some UI elements

--- Assets/Scripts/UI/PixelatedButtonContent.cs
+++ Assets/Scripts/UI/PixelatedButtonContent.cs
@@ -12,20 +12,27 @@ namespace MD.UI
 
         private Vector3 offsetVect;
 
+        private bool isPressed;
+
         void Start()
         {
             offsetVect = new Vector3(0f, offset, 0f);
             button.OnPress += Lower;
             button.OnRelease += Raise;
+            isPressed = false;
         }
 
         public void Lower()
         {
+            if (isPressed) return;
+
             gameObject.transform.position -= offsetVect;
+            isPressed = true;
         }
 
         public void Raise()
         {
+            isPressed = false;
             gameObject.transform.position += offsetVect;
         }
     }
