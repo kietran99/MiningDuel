99b119adb7af0b6cddd179960520e301e9f54d3e
Viet <vietvo151@.gmail.com>
2020-10-21 16:58:40 +0700

    added dropping gem when explode

---
+++ Assets/Scripts/Diggable/Gem/DropObtain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropObtain : MonoBehaviour
+{
+    [SerializeField]
+    private int value = 1;
+    [SerializeField]
+    private bool canObtain;
+    void Start()
+    {
+        canObtain = false;
+        Invoke("EnableObtain",1f);
+    }
+    
+    private void EnableObtain()
+    {
+        canObtain = true;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(Constants.PLAYER_TAG) || !canObtain) return;
+        IScoreManager scoreManager;
+        bool exist = ServiceLocator.Resolve<IScoreManager>(out scoreManager);
+        if (exist) scoreManager.IncreaseScore(value);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Diggable/Projectile/Explosion.cs
+++ Assets/Scripts/Diggable/Projectile/Explosion.cs
@@ -15,6 +15,12 @@ namespace MD.Diggable.Projectile
         [SerializeField]
         private SpriteRenderer spriteRenderer = null;
 
+        [SerializeField]
+        private GameObject droppingGemPrefab = null;
+
+        [SerializeField]
+        private float maxExplosionForce = 50f;
+
         private ITimer timer = null;
 
         private bool canCollide = false;
@@ -37,31 +43,53 @@ namespace MD.Diggable.Projectile
             {
                 timer.Stop();
                 Explode();
+                Destroy(gameObject);
             }     
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!canCollide || !other.CompareTag(Constants.PLAYER_TAG)) return;
-
-            Explode();
+            ExplodeWithPlayer(other.transform.position);
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
             if (!other.CompareTag(Constants.PLAYER_TAG)) return;
-            if (isPlayerCollidedBoom)
-            {
-                Debug.Log("Bùm");
-            }
             canCollide = true;
         }
 
+        private void ExplodeWithPlayer(Vector2 center)
+        {
+            IScoreManager scoreManager;
+            GameObject droppingGem;
+            bool exist = ServiceLocator.Resolve<IScoreManager>(out scoreManager);
+            if (!exist) return;
+            int numOfGem = Mathf.FloorToInt(scoreManager.GetCurrentScore()*stats.GemDropPercentage/100f);
+            scoreManager.DecreaseScore(numOfGem);
+            for (int i = 0; i< numOfGem; i++)
+            {
+                droppingGem = Instantiate(droppingGemPrefab, center, Quaternion.identity);
+                droppingGem.GetComponent<Rigidbody2D>().AddForce(GetExplosionForce()*GetExplosionDirection());
+            }
+            EventSystems.EventManager.Instance.TriggerEvent(new ExplodeData());
+            Destroy(projectileObject);
+        }
         private void Explode()
         {
-            isPlayerCollidedBoom = true;
             Destroy(projectileObject);
-            EventSystems.EventManager.Instance.TriggerEvent(new ExplodeData());
+        }
+
+        private float GetExplosionForce()
+        {
+            return Random.Range(1f,maxExplosionForce);
+        }
+        private Vector2 GetExplosionDirection()
+        {
+            Vector2 randomDir = Vector2.zero;
+            randomDir.x= Random.Range(-1f,1f);
+            randomDir.y = Random.Range(-1f,1f);
+            return randomDir.normalized;
         }
     }
 }
