6a854ae1a08a267c3b8695fe8b35a12a11866177
kietran99 <kietran99@gmail.com>
2020-11-21 23:58:46 +0700

    + Add dig progress gauge sprites + Implement dig progress gauge partial filling

---
+++ Assets/Scripts/VFX/DigProgressGauge.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MD.VisualEffects
+{
+    public class DigProgressGauge : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform fillArea = null;
+        
+        public void Fill(int cur, int max)
+        {
+            float fraction = (float) cur / (float) max;
+            Debug.Log("Fraction to dig successful: " + fraction);
+            fillArea.localScale = new Vector3(fraction, fillArea.localScale.y, 1f);
+            fillArea.localPosition = new Vector3(-1f + fraction, fillArea.localPosition.y, fillArea.localPosition.z);
+        }
+    }
+}
