637916358fe060be3c1788233004504ff2045fc8
Viet <vietvo151@.gmail.com>
2020-11-28 09:32:34 +0700

    implement FMS

---
+++ Assets/Scripts/FMS/State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMSState
+{
+    public enum STATE {FINDPLAYER, RUN, THROWBOMB, DIGBOMB, DIGGEM, FINDGEM, FINDBOMB, WANDER}
+    public enum EVENT {ENTER, UPDATE, EXIT}
+
+    public STATE name;
+    public EVENT stage;
+    public FMSState nextState;
+
+    public FMSState()
+    {
+        stage = EVENT.ENTER;
+    }
+    public virtual void Enter() {stage = EVENT.UPDATE;}
+    public virtual void Update() {}
+    public virtual void Exit() {stage = EVENT.EXIT;}
+
+    public FMSState Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+        return this;
+    }
+}
