153f1ef8b7997252bbec7828f763c393a3ce22d9
kietran99 <kietran99@gmail.com>
2020-12-02 10:57:59 +0700

    + Add documentation for IMap methods

--- Assets/Scripts/Diggable/Core/IMap.cs
+++ Assets/Scripts/Diggable/Core/IMap.cs
@@ -5,15 +5,42 @@
     /// </summary>
     public interface IMap
     {
+        /// <summary>
+        /// Get tile data at position ( <paramref name="x"/>, <paramref name="y"/>)
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="data">Output tile data</param>
+        /// <returns>Whether a tile at ( <paramref name="x"/>, <paramref name="y"/>) is valid</returns>
         bool TryGetAt(int x, int y, out ITileData data);
         bool TrySetAt(int x, int y, ITileData data);
         bool TryReduceAt(int x, int y, int reduceVal);
-        /// <exception cref="MD.Map.Core.InvalidTileException">H</exception>
+        /// <summary>
+        /// Set tile data at position ( <paramref name="x"/>, <paramref name="y"/>)
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="data">Input tile data</param>
+        /// <exception cref="MD.Map.Core.InvalidTileException"></exception>
         void SetAt(int x, int y, ITileData data);
-        /// <exception cref="MD.Map.Core.InvalidTileException">J</exception>
+        /// <summary>
+        /// Reduce remaining dig(s) at position ( <paramref name="x"/>, <paramref name="y"/>)
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="reduceVal">Reduce value</param>
+        /// <exception cref="MD.Map.Core.InvalidTileException"></exception>
         void ReduceAt(int x, int y, int reduceVal);
-        /// <exception cref="MD.Map.Core.InvalidTileException">J</exception>
-        bool IsEmptyAt(int x, int y);       
+        /// <summary>
+        /// Check whether position ( <paramref name="x"/>, <paramref name="y"/>) has no diggable
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <exception cref="MD.Map.Core.InvalidTileException"></exception>
+        bool IsEmptyAt(int x, int y);   
+        /// <summary>
+        /// Logging method for debugging purpose
+        /// </summary>    
         void Log();
     }
 }
--- Assets/Scripts/Diggable/Core/MapTester.cs
+++ Assets/Scripts/Diggable/Core/MapTester.cs
@@ -1,40 +1,19 @@
-﻿using MD.Map.Core;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace MD.Diggable.Core
+namespace MD.Map.Core
 {
     public class MapTester : MonoBehaviour
     {
         void Start()
         {
-            //IMap map = new Map(
-            //    new Tile[4]
-            //    {
-            //        new Tile(1, 2, new TileData(DiggableType.CommonGem)),
-            //        new Tile(-1, 3, new TileData(DiggableType.UncommonGem)),
-            //        new Tile(2, -4, new TileData(DiggableType.RareGem)),
-            //        new Tile(-4, -5, new TileData(DiggableType.CommonGem))
-            //    }
-            //);
-
-            //map.TrySet(-4, -5, new TileData(DiggableType.Empty));
-            
-            //map.Log();
-
-            //if (map.TryGet(2, -4, out TileData data))
-            //{
-            //    Debug.Log(data);
-            //}
-
             IMap map = new DefaultMap();
             try
             {
                 map.SetAt(0, 0, null);
-                
             }
-            catch
+            catch (InvalidTileException)
             {
-                Debug.Log("Catch");
+                Debug.LogError("Invalid tile");
             }
         }
     }
