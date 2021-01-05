fd7e5ac121df2c94329000ff50b5510efa9ed2be
kietran99 <kietran99@gmail.com>
2021-01-05 23:45:20 +0700

    * Rename ScoreMultiplier to ScoreManager

--- Assets/Scripts/Character/ScoreManager.cs
+++ Assets/Scripts/Character/ScoreManager.cs
@@ -1,6 +1,8 @@
-﻿using Mirror;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class ScoreMultiplier : NetworkBehaviour
+public class ScoreManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
