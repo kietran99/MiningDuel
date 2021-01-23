506507f69168f78f3e22eba78a48b4ec9c15a6bf
Viet <vietvo151@.gmail.com>
2021-01-23 17:58:02 +0700

    *Rework sonar mechanic

---
+++ Assets/Scripts/UI/Sonar/SonarController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarController : MonoBehaviour
+{
+    Transform player = null;
+
+    void LateUpdate()
+    {
+        if (player == null)
+        {
+            if (!ServiceLocator.Resolve(out MD.Character.Player player)) return;
+            this.player = player.transform;
+        }
+        transform.position = player.position;
+    }
+}
