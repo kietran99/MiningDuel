23f128584f9285492c17d8873f0e9b0a0b18ce62
kietran99 <kietran99@gmail.com>
2021-02-23 13:11:19 +0700

    + Add Dust Effect on movement

---
+++ Assets/Scripts/VFX/MovementDustEffectController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MD.VisualEffects
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class MovementDustEffectController : MonoBehaviour
+    {
+        private ParticleSystem dustEffect = null;
+
+        private void Start()
+        {
+            dustEffect = GetComponent<ParticleSystem>();
+            gameObject.AddComponent<EventSystems.EventConsumer>().StartListening<UI.JoystickDragData>(ToggleEffect);
+        }
+
+        private void ToggleEffect(UI.JoystickDragData joystickDragData)
+        {
+            if (joystickDragData.InputDirection.Equals(Vector2.zero))
+            {
+                dustEffect.Stop();
+                return;
+            }
+           
+            if (!dustEffect.isPlaying)
+            {
+                dustEffect.Play();
+            }            
+        }
+    }
+}
