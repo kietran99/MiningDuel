5d161307af198ea7e1f82fd6b5aaa82bc91b7356
kietran99 <kietran99@gmail.com>
2021-03-25 12:12:35 +0700

    * Fix Throw input on raycasting UI

---
+++ Assets/Scripts/Event/MirrorEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace EventSystems
+{
+    public class MirrorEventConsumer : EventConsumer
+    {
+        public new static MirrorEventConsumer Attach(GameObject listenerGO) => listenerGO.AddComponent<MirrorEventConsumer>();
+
+        protected override void OnDestroy()
+        {
+            if (!GetComponent<Mirror.NetworkIdentity>().hasAuthority)
+            {
+                return;
+            }
+    
+            base.OnDestroy();
+        }
+    }
+}
