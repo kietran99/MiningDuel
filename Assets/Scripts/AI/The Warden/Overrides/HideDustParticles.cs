95b74f0d3c0ba19675d72305009637ca4ce58cdb
kietran99 <kietran99@gmail.com>
2021-03-31 23:59:12 +0700

    + Show/Hide dust particles depends on The Warden chase behaviour

---
+++ Assets/Scripts/AI/The Warden/Overrides/HideDustParticles.cs
@@ -0,0 +1,14 @@
+﻿using MD.AI.BehaviourTree;
+using UnityEngine;
+
+namespace MD.AI.TheWarden
+{
+    public class HideDustParticles : BTLeaf
+    {
+        protected override BTNodeState DecoratedTick(GameObject actor, BTBlackboard blackboard)
+        {
+            actor.GetComponentInChildren<ParticleSystem>().Stop();
+            return BTNodeState.SUCCESS;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/The Warden/Overrides/ShowDustParticles.cs
@@ -0,0 +1,14 @@
+﻿using MD.AI.BehaviourTree;
+using UnityEngine;
+
+namespace MD.AI.TheWarden
+{
+    public class ShowDustParticles : BTLeaf
+    {
+        protected override BTNodeState DecoratedTick(GameObject actor, BTBlackboard blackboard)
+        {
+            actor.GetComponentInChildren<ParticleSystem>().Play();
+            return BTNodeState.SUCCESS;
+        }
+    }
+}
