6a560545f2c7e00680ef409806bf78e353740fd8
kietran99 <kietran99@gmail.com>
2021-04-01 15:55:17 +0700

    + Reset The Warden scan range after each attack

---
+++ Assets/Scripts/AI/The Warden/Overrides/ResetScanRange.cs
@@ -0,0 +1,14 @@
+﻿using MD.AI.BehaviourTree;
+using UnityEngine;
+
+namespace MD.AI.TheWarden
+{
+    public class ResetScanRange : BTLeaf
+    {
+        protected override BTNodeState DecoratedTick(GameObject actor, BTBlackboard blackboard)
+        {
+            blackboard.Set<float>(WardenMacros.DELTA_CHASE_RANGE, 0f);
+            return BTNodeState.SUCCESS;
+        }
+    }
+}
