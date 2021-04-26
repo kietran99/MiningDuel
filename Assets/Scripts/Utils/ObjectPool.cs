3bc67ec04402e02de4894600b21e7799719819a4
kietran99 <kietran99@gmail.com>
2021-04-26 10:59:19 +0700

    + Allow extendable property for object pools

--- Assets/Scripts/Utils/ObjectPool.cs
+++ Assets/Scripts/Utils/ObjectPool.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ObjectPool : MonoBehaviour, IObjectPool
 {
     [SerializeField]
+    private bool extendable = false;
+
+    [SerializeField]
     private int capacity = 1;
 
     [SerializeField]
@@ -36,14 +39,25 @@ public class ObjectPool : MonoBehaviour, IObjectPool
 
     public GameObject Pop()
     {
-        if (pooledObjects.Count == 0) return null;
+        if (pooledObjects.Count == 0)
+        {
+            if (extendable)
+            {
+                GameObject newObj = Instantiate(objectToPool, parent);
+                freeObjects.Enqueue(newObj);
+                newObj.SetActive(true);
+                return newObj;
+            }
+
+            return null;
+        } 
 
         GameObject obj = pooledObjects.Dequeue();
         freeObjects.Enqueue(obj);
         obj.SetActive(true);
         return obj;
     }
-
+    
     //if use multiple pools in one script, object to pool's name need to be a distinct value
     //push if is a obj of pool
     public void Push(GameObject gameObj)
@@ -66,4 +80,31 @@ public class ObjectPool : MonoBehaviour, IObjectPool
     }
     
     public (GameObject item, int idx) LookUp(Predicate<GameObject> condition) => freeObjects.ToArray().LookUp(condition);
+
+// #if UNITY_EDITOR
+//     private List<GameObject> testActiveObjs = new List<GameObject>();
+
+//     void Update() 
+//     { 
+//         if (Input.GetKeyDown(KeyCode.M)) 
+//         {
+//             testActiveObjs.Add(Pop()); 
+//         }
+
+//         else if (Input.GetKeyDown(KeyCode.P))
+//         {
+//             if (testActiveObjs.Count <= 0) return;
+
+//             var randObj = testActiveObjs[UnityEngine.Random.Range(0, testActiveObjs.Count)];
+//             Push(randObj);
+//             testActiveObjs.Remove(randObj);
+//         }
+
+//         else if (Input.GetKeyDown(KeyCode.R)) 
+//         {
+//             Reset(); 
+//             testActiveObjs.Clear();
+//         }
+//     }
+// #endif
 }
