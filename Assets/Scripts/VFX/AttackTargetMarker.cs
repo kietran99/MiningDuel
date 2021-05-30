334b2e322a265dd9775f8cdb08622f8031a4bc45
kietran99 <kietran99@gmail.com>
2021-05-31 01:13:38 +0700

    * Separate Dig target marker and Attack target marker into different classes + Animate Attack target marker

---
+++ Assets/Scripts/VFX/AttackTargetMarker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace MD.VisualEffects
+{
+    public class AttackTargetMarker : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer spriteRenderer = null;
+
+        [SerializeField]
+        private Animator animator = null;
+
+        private Vector2 targetPos;
+
+        private void Start()
+        {
+            EventSystems.EventConsumer.Attach(gameObject).StartListening<Character.AttackTargetChangeData>(ToggleMode);
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = targetPos;
+        }
+
+        private void ToggleMode(Character.AttackTargetChangeData data)
+        {
+            if (!data.attackable)
+            {
+                animator.enabled = false;
+                spriteRenderer.sprite = null;
+                return;
+            }
+
+            targetPos = data.targetPos;
+            animator.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/VFX/DigTargetMarker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MD.VisualEffects
+{
+    public class DigTargetMarker : MonoBehaviour
+    {
+        private Transform target;
+
+        private void Start()
+        {
+            target = ServiceLocator.Resolve<Character.Player>().Match(err => transform, player => player.transform);
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = new Vector3(Mathf.FloorToInt(target.position.x) + .5f, Mathf.FloorToInt(target.position.y) + .5f, 0f);
+        }
+    }
+}
